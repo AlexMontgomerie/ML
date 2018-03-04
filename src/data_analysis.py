55b01d1fa145176359df90602604d89142c58b8d
Alex Montgomerie <AlexMontgomerie@github.com>
2018-03-04 23:02:39 +0000

    added data analysis

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -4,6 +4,7 @@ from sklearn.metrics import accuracy_score
 import numpy as np
 import seaborn as sns
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 NUM_FEATURE = 11
 
@@ -12,41 +13,69 @@ def get_data():
   #return np.delete(wine_data,(0),axis=0)
   return wine_data
 
-def get_correlation_matrix(data):
+def normalise(data):
+  return (data - data.mean())/data.std() 
+
+def normalise_all(data):
+  for i in range(NUM_FEATURE):
+    data[:,i] = normalise(data[:,i])
+  return data
+
+def get_correlation_matrix(data,fig_num):
   data = np.delete(data,-1,axis=1)
   
   df = pd.DataFrame(data=data)
   
-  fig = plt.figure()
+  fig = plt.figure(fig_num)
   ax = fig.add_subplot(111)  
   cax = ax.matshow(df.corr())
   fig.colorbar(cax)
   return
 
-def correlation_plot(x,y):
-    
-
-def visualise_data(x,y):
+def visualise_data(x,y,fig_num):
   sz = np.array([0.2 for x in range(len(y))])
-  fig = plt.figure()
+  fig = plt.figure(fig_num)
   for i in range(NUM_FEATURE):
-    ax = fig.add_subplot(111)
-    plt.scatter(np.transpose(x[:,i]),y,sz)  
+    ax = fig.add_subplot(2,6,i+1)
+    x_norm = normalise(x[:,i])
+    ax.scatter(x_norm,y,sz)  
+    ax.set_ylim([0,10])
+    ax.set_xlim(xmin=0)
+        
   return
 
-def get_data_plot(data):
-  #remove 
+def get_data_plot(data,fig_num):
+  data = np.delete(data,(0),axis=0)
+   
+  x,y = np.hsplit(data,[NUM_FEATURE])
+  
+  visualise_data(x,y,fig_num)
+  return
+
+
+def get_corr_coef(x,y):
+  coef_array = np.corrcoef(np.transpose(x),np.transpose(y)) 
+  return coef_array[0,1]
+  
+
+def get_data_output_correlation_plot(data,fig_num):
   data = np.delete(data,(0),axis=0)
   x,y = np.hsplit(data,[NUM_FEATURE])
-  visualise_data(x,y)
+  x = normalise_all(x)
+  corr = np.array([get_corr_coef(x[:,i],y) for i in range(NUM_FEATURE)])
+  x_axis = np.arange(NUM_FEATURE)
+
+  plt.figure(fig_num)
+  plt.bar(x_axis,corr)
   return
 
-def get_data_output_correlation_plot(data):
 
 if __name__=="__main__":
   data = get_data()
-  get_correlation_matrix(data)
-  get_data_plot(data)
+  print(data.shape)
+  get_correlation_matrix(data,1)
+  get_data_plot(data,2)
+  get_data_output_correlation_plot(data,3)
 
   plt.show()
 
